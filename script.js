fc57856ccead2de205251564b743b10629e1a774
J2021T <80201567+J2021T@users.noreply.github.com>
2021-04-30 04:26:27 -0400

    created repository

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment code here
+var length = Number(prompt("What is your desired character length? Please enter a value from 8 to 128.")) 
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
